bcc845b8b115f50caaeffec19741a4f3bb389782
Khue Tran <khuebtran03@gmail.com>
2025-03-03 14:22:30 -0500

    Cleaned APCSSA Null Table and Created Internal Data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,13 @@
-## code to prepare `DATASET` dataset goes here
+# Load required packages
+library(tidyverse)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# Read and process the dataset
+FirstNullAPCSSA <- read_csv("data-raw/NullDistributionAPCSSA.csv") %>%
+  mutate(
+    SD_CRA = sqrt(V_CRA),
+    SD_RCA = sqrt(V_RCA)
+  ) %>%
+  select(I, J, K, E_CRA, SD_CRA, E_RCA, SD_RCA)
+
+# Save the cleaned dataset internally
+usethis::use_data(FirstNullAPCSSA, internal = TRUE, overwrite = TRUE)
