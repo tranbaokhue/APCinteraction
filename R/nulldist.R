aa079e6789e8ba431f84a887dc518ebaaee5bce8
Khue Tran <khuebtran03@gmail.com>
2025-12-16 12:02:55 +1100

    Transition to dontrun for sim examples

--- R/nulldist.R
+++ R/nulldist.R
@@ -33,7 +33,7 @@
 #' @returns A data frame with the null mean and standard deviation for the two test statistics (APCCRA and APCRCA) that will get standardized into APCSSA and a numeric vector with length equals to the `numSim` of all APCSSA statistics on the null data sets.
 #'
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' sim_nullAPCSSA(2, 2, 2, 5000)  # This should take only a few seconds
 #' }
 #'
@@ -100,7 +100,7 @@ sim_nullAPCSSA <- function(i, j, k,
 #' @returns A data frame with the null mean and standard deviation for the two test statistics (APCCRM and APCRCM) that will get standardized into APCSSM and a numeric vector with length equals to the `numSim` of all APCSSM statistics on the null data sets.
 #'
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' sim_nullAPCSSA(2, 2, 2, 5000)  # This should take only a few seconds
 #' }
 #'
